2e1db534df8033b51aab8c5d50f7cfce2ea05b01
Nicoaedhel <nicoa.cn@gmail.com>
2016-02-12 23:19:14 +0100

    Edit chat

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -1,12 +1,55 @@
 var socket = io.connect('http://localhost:8000');
 
-$('form').submit(function(event){
+// Connect 
+$('#connect').click(function(event){
 	event.preventDefault();
-	socket.emit('chat message', $('#m').val());
-	$('#m').val('');
+	socket.emit('chat connect', {
+		username : '@'+$('form.insert-connect').find('[name="username"]').val(),
+		mail : $('form.insert-connect').find('[name="mail"]').val()
+	});
 	return false;
 });
 
+// If connect
+socket.on('chat connect', function(user){
+	$('.panel-start').addClass('removed');
+	$('.menu').removeClass('removed');
+	$('.content').removeClass('removed');
+	$('#m').focus();
+});
+
+
+
+
+
+
+
+// Add users
+socket.on('chat users', function(users){
+	$('#users').empty();
+	for (user in users) {
+		$('#users').append('<img src="'+users[user].avatar+'" class="avatar"/>');
+		$('#users').append($('<li>').text(users[user].username));
+	};
+	
+});
+
+
+
+
+
+
+
+// msg to server
+$('form.insert-text').submit(function(event){
+	event.preventDefault();
+	socket.emit('chat message',$('#m').val());
+	$('#m').val('');
+	$('#m').focus();
+	return false;
+});
+
+// display msg
 socket.on('chat message', function(msg){
 	$('#messages').append($('<li>').text(msg));
 });
--- server/run.js
+++ server/run.js
@@ -5,12 +5,29 @@ httpServer = http.createServer();
 httpServer.listen(8000, function(){
 	console.log('Server running at http://localhost:8000');
 });
-
+var users = [];
 var io = require('socket.io')(httpServer);
+var gravatar = require('gravatar');
+
+
 io.on('connection', function(socket){
-	console.log('Nouveau utilisateur');
-	socket.on('chat message', function(msg){
-		console.log('Nouveau message : '+msg);
-		io.emit('chat message', msg);
+	
+	var me;
+
+	// Connected / Disconect
+	socket.on('chat connect', function(user){
+		me = user;
+		me.avatar = gravatar.url(me.mail, {s: '20', r: 'x', d: 'retro'}, true);
+		users.push(me);
+		socket.emit('chat connect');
+		io.emit('chat message', 'ChatSquare : hello '+me.username+' !');
+		io.emit('chat users', users);
 	});
+
+
+	// Post msg
+	socket.on('chat message', function(msg){
+		io.emit('chat message', me.username +' : '+msg);
+	});
+
 });
